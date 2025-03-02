cb92fd78131bbb432aefa68041b0d46b25c0d419
Juan Ceron <juanmiguelceronocampo@gmail.com>
2025-03-01 20:14:04 -0500

    Commit inicial

--- handler.py
+++ handler.py
@@ -1,9 +1,11 @@
 import json
 
 
-def info(event, context):
+def my_info(event, context):
     body = {
-        "nombre": "Juan Miguel Ceron Ocampo", "codigo": "240220232013", "fecha nacimiento": "17/07/2004", "edad": "20 años", "correo": "juan.ceron.0368@eam.edu.co", "direccion": "La Tebaida, Quindio"
+        "nombre": "thomas garcia", "edad": "19", "cedula": "1094894584", 
+        "direccion": "calle 25N #13-41", "profesion": "estudiante", 
+        "telefono": "3157480639", "email": "garcia.thomas.9548@eam.edu.co"
     }
 
     response = {"statusCode": 200, "body": json.dumps(body)}
